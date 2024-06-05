73d94185c37c379421da2fbb1e88c2e0ceed4f6a
João Santos <santos42jv@gmail.com>
2024-06-05 20:08:47 -0300

    initial commit

---
+++ src/main/java/com/unicap/capnet/CapnetApplication.java
@@ -0,0 +1,13 @@
+package com.unicap.capnet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapnetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapnetApplication.class, args);
+	}
+
+}
